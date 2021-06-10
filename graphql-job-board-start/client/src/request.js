5cba006a0c4379569402140bb6faab2cb064c15e
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-06-10 13:08:11 +0100

    query variables: fetching a job

--- graphql-job-board-start/client/src/request.js
+++ graphql-job-board-start/client/src/request.js
@@ -1,5 +1,30 @@
 const endpointUrl = "http://localhost:9000/graphql";
 
+export async function loadJob(id) {
+  const response = await fetch(endpointUrl, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      query: `
+      query JobQuery($id: ID!) {
+        job(id: $id){
+          id
+          title
+          description
+          company {
+            id
+            name
+          }
+        }
+      }
+      `,
+      variables: { id },
+    }),
+  });
+  const responsebody = await response.json();
+  return responsebody.data.job;
+}
+
 export async function loadJobs() {
   const response = await fetch(endpointUrl, {
     method: "POST",
